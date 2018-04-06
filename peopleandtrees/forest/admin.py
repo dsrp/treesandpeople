b6f51d7248c062d97f8f89cfec384bd64535ceff
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-04-06 23:04:44 +0100

    Initial models and admin.

--- peopleandtrees/forest/admin.py
+++ peopleandtrees/forest/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Species, Culture, CultureSpecies, Forest, ForestCulture
+
+
+class CultureSpeciesInline(admin.TabularInline):
+    model = CultureSpecies
+
+
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Culture)
+class CultureAdmin(admin.ModelAdmin):
+    inlines = [
+        CultureSpeciesInline
+    ]
+
+
+class ForestCultureInline(admin.TabularInline):
+    model = ForestCulture
+
+
+@admin.register(Forest)
+class ForestAdmin(admin.ModelAdmin):
+    inlines = [
+        ForestCultureInline
+    ]
--- peopleandtrees/forest/models.py
+++ peopleandtrees/forest/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class NamedMixin(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class Species(NamedMixin):
+    plants_per_area = models.FloatField()
+
+
+class Culture(NamedMixin):
+    species = models.ManyToManyField(Species, through='CultureSpecies')
+
+
+class CultureSpecies(models.Model):
+    culture = models.ForeignKey(Culture, on_delete=models.CASCADE)
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+
+    ratio = models.FloatField()
+
+
+class Forest(NamedMixin):
+    pass
+
+
+class ForestCulture(models.Model):
+    culture = models.ForeignKey(Culture, on_delete=models.CASCADE)
+    forest = models.ForeignKey(Forest, on_delete=models.CASCADE)
+
+    area = models.FloatField()
