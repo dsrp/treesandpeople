d9d6e1baba90b0f04f644ddb18b4fe55893bda42
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-04-06 23:29:18 +0100

    Refine stuff.

--- peopleandtrees/forest/models.py
+++ peopleandtrees/forest/models.py
@@ -13,11 +13,19 @@ class NamedMixin(models.Model):
 
 
 class Product(NamedMixin):
-    pass
+    price = models.FloatField(
+        help_text=_('Market sales price per metric ton.')
+    )
 
 
 class Species(NamedMixin):
-    plants_per_area = models.FloatField()
+    plants_per_area = models.FloatField(
+        help_text=_('Per square meter.')
+    )
+    labour_per_plant = models.FloatField(
+        help_text=_('Hours of labour per plant, per year.')
+    )
+
     products = models.ManyToManyField(Product, through='SpeciesProduct')
 
 
@@ -25,7 +33,9 @@ class SpeciesProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     species = models.ForeignKey(Species, on_delete=models.CASCADE)
 
-    yield_per_plant = models.FloatField()
+    yield_per_plant = models.FloatField(
+        help_text=_('In metric tons per year.')
+    )
 
     class Meta:
         verbose_name = _('product')
@@ -40,7 +50,10 @@ class CultureSpecies(models.Model):
     culture = models.ForeignKey(Culture, on_delete=models.CASCADE)
     species = models.ForeignKey(Species, on_delete=models.CASCADE)
 
-    ratio = models.FloatField()
+    ratio = models.FloatField(
+        help_text=_('Fraction of monoculture equivalent for this species.')
+        # TODO: Validate value > 0.0 and <= 1.0
+    )
 
 
 class Forest(NamedMixin):
