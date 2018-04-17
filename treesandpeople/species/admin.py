c9950d0b69475eea72bd64ae4982e25f5af22559
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-04-17 22:23:07 +0100

    Full species admin.

--- treesandpeople/species/admin.py
+++ treesandpeople/species/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
 from base.admin import InlineBase
-from .models import Species, SpeciesProduction
+
+from .models import (
+    SpeciesCulture, Species,
+    SpeciesCostsCategory, SpeciesCosts,
+    SpeciesLabourCategory, SpeciesLabour,
+    SpeciesProduction
+)
+
+
+class SpeciesCultureInline(InlineBase):
+    model = SpeciesCulture
+
+
+class SpeciesCostsInline(InlineBase):
+    model = SpeciesCosts
+
+
+class SpeciesLabourInline(InlineBase):
+    model = SpeciesLabour
 
 
 class SpeciesProductionInline(InlineBase):
@@ -11,5 +29,18 @@ class SpeciesProductionInline(InlineBase):
 @admin.register(Species)
 class SpeciesAdmin(admin.ModelAdmin):
     inlines = [
+        SpeciesCultureInline,
+        SpeciesCostsInline,
+        SpeciesLabourInline,
         SpeciesProductionInline
     ]
+
+
+@admin.register(SpeciesCostsCategory)
+class SpeciesCostsCategoyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpeciesLabourCategory)
+class SpeciesLabourCategoryAdmin(admin.ModelAdmin):
+    pass
